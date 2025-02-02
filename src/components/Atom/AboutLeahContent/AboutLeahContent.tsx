9f87d3bad7efe458645361b9a6d533759be6e3e6
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2025-02-02 18:34:51 +0330

    final about-leah commit

---
+++ src/components/Atom/AboutLeahContent/AboutLeahContent.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import { contentAbout } from './data';
+import { Fragment } from 'react';
+
+const AboutLeahContent = () => {
+    return (
+        <article>
+            {contentAbout.map((item, index) => (
+                <Fragment key={item.id}>
+                    <div className="flex flex-col md:flex-row items-center md:items-start gap-6">
+
+                        <Image
+                            src={`/images/Leah/${item.src}`}
+                            alt={`Professional Journey of Leah Taylor Roy - ${item.title}`}
+                            width={400}
+                            height={500}
+                            className="rounded-lg shadow-lg"
+                            loading="lazy"
+                            sizes="(max-width: 768px) 400px, 600px"
+                        />
+
+
+                        <article className="md:w-1/2 text-center md:text-left">
+                            <h2 className="text-2xl font-bold text-primary mb-4">
+                                {item.title}
+                            </h2>
+                            {item.content.split('.').map((sentence, index) => (
+                                <p key={index} className="text-lg text-gray-700 mb-4">
+                                    {sentence}.
+                                </p>
+                            ))}
+                        </article>
+                    </div>
+                    {index < contentAbout.length - 1 &&  <hr className="my-6 border-gray-300" />}
+                </Fragment>
+            ))}
+        </article>
+    );
+};
+
+export default AboutLeahContent;
---
+++ src/components/Molecule/AboutLeah/AboutLeah.tsx
@@ -0,0 +1,27 @@
+import Summary from "@/components/Atom/Summary/Summary";
+
+// images
+import LeahImage from "../../../../public/images/Leah/leah-image.webp";
+import AboutLeahContent from "@/components/Atom/AboutLeahContent/AboutLeahContent";
+
+const AboutLeah = () => {
+    return (
+        <section>
+            <div className="py-36 px-8">
+                <Summary
+                    src={LeahImage}
+                    title="Meet Leah Taylor Roy"
+                    content="Born and raised in York Region, I now live in Aurora with my husband, Peter Roy. With a strong family background in public service and community leadership, I am committed to being a strong voice for Aurora—Oak Ridges—Richmond Hill in Parliament. I hold degrees from the University of Toronto and Harvard and have worked at the World Bank, McKinsey & Company, and in the energy sector. Beyond politics, I am dedicated to community service, supporting various charities and initiatives to improve our region."
+                    href="https://www.ourcommons.ca/members/en/leah-taylor-roy(105024)#work"
+                    buttonContent="Explore Parliamentary Work"
+                    reverse={true}
+                />
+            </div>
+            <div className="bg-white px-8 py-4">
+                <AboutLeahContent />
+            </div>
+        </section>
+    )
+}
+
+export default AboutLeah;
