19f0ec170bee5f26a0b73a70eee9ee74fc92b979
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2025-02-08 04:53:25 +0330

    set isr on dynamic pages

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,24 +1,10 @@
-import { NextPage } from "next";
 import { fetchPublishedNews } from "@/actions/getPublishedNews";
 import News from "@/components/Molecule/News/News";
 
 
-interface NewsItem {
-  id: number;
-  title: string;
-  content: string;
-  index_image_url: string;
-  updated_at: string;
-}
-
-export interface NewsPageProps {
-  news: NewsItem[];
-}
-
-
 export const revalidate = 60; 
 
-const NewsPage:NextPage<NewsPageProps> = async () => {
+const NewsPage = async () => {
 
   const news = await fetchPublishedNews();
 
