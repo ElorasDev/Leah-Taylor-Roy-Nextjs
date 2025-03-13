515b5285f33c618ec842b6214a38d900dba8d3ec
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2025-03-13 23:50:58 +0330

    some change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { SupabaseProvider } from "@/Providers/SupabaseProvider";
 import Layout from "../components/Molecule/Layout/Layout";
 import "./globals.css";
 import ReactQueryProvider from "@/Providers/ReactQueryProvider";
-import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -11,12 +10,12 @@ export default function RootLayout({
   return (
     <html lang="en-US">
       <head>
-        <Script 
-          src="https://tag.simpli.fi/sifitag/fbf0d310-9803-0136-4f23-067f653fa718" 
-          strategy="afterInteractive" 
-        />
       </head>
       <body className="antialiased bg-[#F2F3F4]">
+      <script
+          async
+          src="https://tag.simpli.fi/sifitag/fbf0d310-9803-0136-4f23-067f653fa718" 
+        ></script>
         <ReactQueryProvider>
           <SupabaseProvider>
             <Layout>
